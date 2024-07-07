9de10510d2f331eb8b83e149b8b0c656afd09d32
Yliu-101 <kailiu989@gmail.com>
2024-07-07 12:10:59 -0500

    updated testing to get it to work

--- test_backend.py
+++ test_backend.py
@@ -1,12 +1,22 @@
 import unittest
 from backend import TicTacToe
 
-class TestTicTacToe:
-    def setUp(self):
-         board = [[1,0,1],[1,0,0],[-1,-1,-1]]
-         tac = TicTacToe(board, -1)
-    def test_board():
-           pass     
+# class TestTicTacToe(unittest.TestCase):
+#     def setUp(self):
+#          board = [[1,0,1],[1,0,0],[-1,-1,-1]]
+#          tac = TicTacToe(board, -1)
+#     def test_board():
+#            pass     
+#     def testing123(self):
+#          self.assertEquals(1,1)
+
+class Test_Backend(unittest.TestCase):
+    def test_increment(self):
+        self.assertEqual(4, 4)
+
+    # This test is designed to fail for demonstration purposes.
+    def test_decrement(self):
+        self.assertEqual(2+2.0000000000000001, 4)
+
 if __name__ == '__main__':
     unittest.main()
-    
